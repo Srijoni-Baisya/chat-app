ef9466cd95bf154bc20c63e7f6953f7a2e631d0a
Srijoni-Baisya <75311639+Srijoni-Baisya@users.noreply.github.com>
2021-04-17 12:24:39 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDJHhRvrT61xX7idAacIV0dIRnbJnL67t4",
+    authDomain: "kwitterwebapp-ac82a.firebaseapp.com",
+    databaseURL: "https://kwitterwebapp-ac82a-default-rtdb.firebaseio.com",
+    projectId: "kwitterwebapp-ac82a",
+    storageBucket: "kwitterwebapp-ac82a.appspot.com",
+    messagingSenderId: "312577833010",
+    appId: "1:312577833010:web:d20ad7b08f0868c8b6e42b"
+  };
+  
+firebase.initializeApp(firebaseConfig);
